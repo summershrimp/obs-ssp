f069d28d000a001833526dd1442a8dcddd5f5588
Yibai Zhang <xm1994@gmail.com>
2020-10-10 00:41:32 +0800

    fix(chore): Fix CI build issue due to brew update
    We just follow the official obs-studio deps download link.
    
    Signed-off-by: Yibai Zhang <xm1994@gmail.com>

--- CI/package-macos.sh
+++ CI/package-macos.sh
@@ -24,13 +24,13 @@ FILENAME="obs-ssp-$VERSION.pkg"
 
 echo "[obs-ssp] Modifying obs-ssp.so"
 install_name_tool \
-	-change /usr/local/opt/qt/lib/QtWidgets.framework/Versions/5/QtWidgets \
+	-change /tmp/obsdeps/lib/QtWidgets.framework/Versions/5/QtWidgets \
 		@executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets \
-	-change /usr/local/opt/qt/lib/QtGui.framework/Versions/5/QtGui \
+	-change /tmp/obsdeps/lib/QtGui.framework/Versions/5/QtGui \
 		@executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui \
-	-change /usr/local/opt/qt/lib/QtCore.framework/Versions/5/QtCore \
+	-change /tmp/obsdeps/lib/QtCore.framework/Versions/5/QtCore \
 		@executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore \
-	-change /usr/local/opt/qt/lib/QtNetwork.framework/Versions/5/QtNetwork \
+	-change /tmp/obsdeps/lib/QtNetwork.framework/Versions/5/QtNetwork \
 		@executable_path/../Frameworks/QtNetwork.framework/Versions/5/QtNetwork \
 	-change /tmp/obsdeps/bin/libavcodec.58.dylib \
 		@executable_path/../Frameworks/libavcodec.58.dylib \
